a4082ec1fc233bb76102c35edeac10087dd649ac
Usmanhanafi <67598657+usmanhanafi@users.noreply.github.com>
2021-07-07 21:53:29 +0700

    Delete create-snowpack-app/app-template-blank-typescript directory

--- create-snowpack-app/app-template-blank-typescript/src/index.ts
+++
@@ -1,9 +0,0 @@
-const counter = document.querySelector('#counter') as HTMLSpanElement;
-let seconds = 0;
-
-setInterval(() => {
-  seconds += 1;
-  counter.textContent = seconds.toString();
-}, 1000);
-
-export {};
